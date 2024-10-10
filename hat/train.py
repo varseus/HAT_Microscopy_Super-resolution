c51218f64c7891ab1f412dbdba30ad3523ef3ef2
Varun Gupta <varseus@gmail.com>
2024-10-10 01:48:46 -0400

    Initial SEM dataset and pretrained GAN/SR4 models

--- hat/train.py
+++ hat/train.py
@@ -5,6 +5,9 @@ import hat.archs
 import hat.data
 import hat.models
 from basicsr.train import train_pipeline
+# import ssl
+
+# ssl._create_default_https_context = ssl._create_stdlib_context
 
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
